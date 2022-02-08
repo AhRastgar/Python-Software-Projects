c46dc12ece532f44223e7f9c08480486c3d35473
AmirhHosein Rastgar <me@amirrastgar.ir>
2022-02-08 13:40:34 +0330

    .

---
+++ Draw a star.py
@@ -0,0 +1,9 @@
+#subtitle
+print ("This project is for Draw stars with size of number.")
+
+#input
+x = int (input ("Enter a number "))
+
+#the operation & output
+for i in range (x):
+    print ("*")
