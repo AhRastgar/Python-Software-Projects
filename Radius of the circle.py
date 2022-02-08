f508e43742827ba13f62e472c8f1066a314ba690
AmirhHosein Rastgar <me@amirrastgar.ir>
2022-02-08 11:36:12 +0330

    .

---
+++ Radius of the circle.py
@@ -0,0 +1,16 @@
+# subtitle
+print ("This project is for Radius of the circle. \n")
+
+# variable
+p = 3.14
+
+# input value
+x = int(input("Enter a number for radius "))
+
+# the operation
+a=x*2
+b=2*p*x
+c=p*x*x
+
+# output
+print ("diameter= " , a , "\n Environment= " , b , "\n Area= " , c)
