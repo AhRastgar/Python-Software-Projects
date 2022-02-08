bc8e4297bd15443e8d8f0c38ed7a5ee8fada50da
AmirhHosein Rastgar <me@amirrastgar.ir>
2022-02-08 12:08:55 +0330

    .

---
+++ X pow Y.py
@@ -0,0 +1,12 @@
+#subtitle
+print ("This project is for x pow y.")
+
+#input
+x = int (input ("Enter a number for x "))
+y = int (input ("Enter a number for y "))
+
+#the operation
+a = pow (x , y)
+
+#output
+print ("x pow y = ", a)
