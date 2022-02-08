9b73f1048f9c92b8195034a46faf05c316905be5
AmirhHosein Rastgar <me@amirrastgar.ir>
2022-02-08 12:04:24 +0330

    .

---
+++ Pow.py
@@ -0,0 +1,13 @@
+# subtitle
+print ("This project is for pow")
+
+# input
+x = int (input ("Enter a number "))
+
+# the operation
+a = x
+b = pow (x,2)
+c = pow (x,3)
+
+# output
+print ("pow 1 = ", a, "\n pow 2 = ", b, "\n pow 3 = ", c)
