8d72b7d88e0abcc5133c79a94d62df887c5e11e2
AmirhHosein Rastgar <me@amirrastgar.ir>
2022-02-08 13:00:44 +0330

    .

---
+++ Product with sum.py
@@ -0,0 +1,15 @@
+#subtitle
+print ("This project is for product with sum.")
+
+#input
+x = int (input ("Enter first number "))
+y = int (input ("Enter second number "))
+
+#the operation
+sum = 0
+
+for i in range (0 , y):
+    sum = sum + x
+
+#output
+print ("Product = ", sum)
