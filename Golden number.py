440853b36e9f4a4a9bb7c6374aef6e525d6827a7
AmirhHosein Rastgar <me@amirrastgar.ir>
2022-02-08 13:35:32 +0330

    .

---
+++ Golden number.py
@@ -0,0 +1,9 @@
+#subtitle
+print ("This project is for golden number.")
+
+#input
+x = int (input ("Enter a number for fuond golden number "))
+
+# the operation & output
+print ("If the number you give is the length, the ratio is = " , x/1.618)
+print ("if the number you give is the width, the ratio is = " , x*1.618)
