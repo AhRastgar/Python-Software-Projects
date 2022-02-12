139967fe8f3bc2988be0fee262acaf578f50df2a
AmirhHosein Rastgar <me@amirrastgar.ir>
2022-02-12 11:40:07 +0330

    .

---
+++ Expansion of pi number.py
@@ -0,0 +1,13 @@
+#subtilte
+print ("This project is for expansion of pi number.")
+
+#input
+x = int (input ("Enter a number "))
+pi = float (3.14)
+
+#the operation
+env = 2 * pi * x
+exp = env / x
+
+#output
+print ("Expansion of pi number is = " , exp)
