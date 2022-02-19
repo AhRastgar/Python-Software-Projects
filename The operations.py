dfba6336583907ae59fa3b51965b27b60b412da5
AmirhHosein Rastgar <me@amirrastgar.ir>
2022-02-19 17:33:21 +0330

    .

--- The operations.py
+++ The operations.py
@@ -2,15 +2,10 @@
 from ast import Num
 from cmath import sqrt
 
-
 print ("This project is for the operations.")
 
 #input
 x = int (input ("Enter a number for number of numbers "))
-
-# #variables
-# import array as arr
-# num = arr.array ('i', [0, 99])
 
 #the operation
 stash = 0
@@ -18,32 +13,6 @@ sum = 0
 avg = 0
 firstInit = 0
 sum2 = 0
-
-# for i in range (0, x):
-#     num = input ("Enter your numbers ")
-#     num = num[i]
-
-#     if firstInit == 0:
-#         min = num[0]
-#         max = num[0]
-
-#         firstInit = 1
-
-#     if num[i] < min:
-#         min = num[i]
-
-#     if num [i] > max:
-#         max = num[i]
-
-# for r in range (num[x]):
-#     sum = sum + num[r]
-
-# avg = sum / x
-
-# for j in range (0, x):
-#     stash = num[j] - avg
-#     sum2 = sum2 + pow (stash, 2)
-#     stdDev = sqrt (sum2 / 5)
 
 for i in range (x):
     b = int (input ("Enter a number "))
